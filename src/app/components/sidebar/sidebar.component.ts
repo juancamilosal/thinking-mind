6aeb7b3b2a4c8be8e7eda347a0afb2ada892a6fa
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-08-10 18:22:34 -0500

    Se ajusta el sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostListener, Output} from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -8,6 +8,7 @@ import { CommonModule } from '@angular/common';
   imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidebar.component.html'
 })
+
 export class SidebarComponent {
   @Output() sidebarStateChange = new EventEmitter<boolean>();
   isOpen = false;
@@ -18,4 +19,24 @@ export class SidebarComponent {
     { path: '/private/accounts-receivable', icon: 'cash', label: 'Cuentas por Cobrar' }
   ];
 
+  constructor(private eRef: ElementRef) {
+  }
+
+  toggleSidebar = () => {
+    this.isOpen = !this.isOpen;
+    this.sidebarStateChange.emit(this.isOpen);
+  }
+
+  onMenuItemClick = ()=> {
+    this.isOpen = false;
+    this.sidebarStateChange.emit(false);
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside= (event: Event)=> {
+    if (this.isOpen && !this.eRef.nativeElement.contains(event.target)) {
+      this.isOpen = false;
+      this.sidebarStateChange.emit(false);
+    }
+  }
 }
