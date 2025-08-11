076b4f2e60703e521aa5b9532b4e8b0f4561d925
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-08-10 21:18:27 -0500

    Se ajusta el sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
-import {Component, ElementRef, EventEmitter, HostListener, Output} from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -8,35 +9,21 @@ import { CommonModule } from '@angular/common';
   imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidebar.component.html'
 })
-
 export class SidebarComponent {
-  @Output() sidebarStateChange = new EventEmitter<boolean>();
-  isOpen = false;
+  isSidebarOpen = false;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  closeSidebar() {
+    this.isSidebarOpen = false;
+  }
+
   menuItems = [
     { path: '/private/clients', icon: 'users', label: 'Clientes' },
     { path: '/private/schools', icon: 'building', label: 'Colegios' },
     { path: '/private/courses', icon: 'book', label: 'Cursos' },
     { path: '/private/accounts-receivable', icon: 'cash', label: 'Cuentas por Cobrar' }
   ];
-
-  constructor(private eRef: ElementRef) {
-  }
-
-  toggleSidebar = () => {
-    this.isOpen = !this.isOpen;
-    this.sidebarStateChange.emit(this.isOpen);
-  }
-
-  onMenuItemClick = ()=> {
-    this.isOpen = false;
-    this.sidebarStateChange.emit(false);
-  }
-
-  @HostListener('document:click', ['$event'])
-  clickOutside= (event: Event)=> {
-    if (this.isOpen && !this.eRef.nativeElement.contains(event.target)) {
-      this.isOpen = false;
-      this.sidebarStateChange.emit(false);
-    }
-  }
 }
--- src/app/layouts/private-layout/private-layout.ts
+++ src/app/layouts/private-layout/private-layout.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-private-layout',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+    imports: [RouterOutlet, SidebarComponent, NgClass],
   templateUrl: './private-layout.html'
 })
-export class PrivateLayout {}
+export class PrivateLayout implements OnInit {
+
+  isSidebarOpen = false;
+  windowWidth = 0;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.windowWidth = window.innerWidth;
+    this.isSidebarOpen = this.windowWidth >= 640;
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.windowWidth = window.innerWidth;
+  }
+}
