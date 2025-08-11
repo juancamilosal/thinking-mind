7db9c0ad6d17c547eb9c87a301509347f36ac9f0
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-08-10 22:35:28 -0500

    Se crea la sección de cursos

--- src/app/layouts/private-layout/pages/schools/schools.ts
+++ src/app/layouts/private-layout/pages/schools/schools.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-schools',
-  imports: [],
-  templateUrl: './schools.html',
-  standalone: true
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './schools.html'
 })
-export class Schools {
+export class Schools implements OnInit {
+  schoolForm!: FormGroup;
+  showForm = false;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.schoolForm = this.fb.group({
+      schoolName: ['', Validators.required],
+      city: ['', Validators.required],
+      address: ['', Validators.required],
+      principalName: ['', Validators.required],
+      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]]
+    });
+  }
+
+  toggleForm() {
+    this.showForm = !this.showForm;
+    if (!this.showForm) {
+      this.schoolForm.reset();
+    }
+  }
+
+  onSubmit() {
+    if (this.schoolForm.valid) {
+      console.log(this.schoolForm.value);
+    } else {
+      this.markFormGroupTouched(this.schoolForm);
+    }
+  }
+
+  private markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+    });
+  }
 }
