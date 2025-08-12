fd5f61f4a06dac5d5e8e693218d47f878a683879
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-08-11 21:40:08 -0500

    Se ajusta el private layout

--- src/app/layouts/private-layout/private-layout.ts
+++ src/app/layouts/private-layout/private-layout.ts
@@ -1,28 +1,36 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import { Component, HostListener, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
-import {NgClass} from "@angular/common";
+import { NgClass, isPlatformBrowser } from "@angular/common";
 
 @Component({
   selector: 'app-private-layout',
   standalone: true,
-    imports: [RouterOutlet, SidebarComponent, NgClass],
+  imports: [RouterOutlet, SidebarComponent, NgClass],
   templateUrl: './private-layout.html'
 })
 export class PrivateLayout implements OnInit {
 
   isSidebarOpen = false;
   windowWidth = 0;
+  isBrowser = false;
 
-  constructor() {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+  }
 
   ngOnInit() {
-    this.windowWidth = window.innerWidth;
-    this.isSidebarOpen = this.windowWidth >= 640;
+    if (this.isBrowser) {
+      this.windowWidth = window.innerWidth;
+      this.isSidebarOpen = this.windowWidth >= 640;
+    }
   }
 
   @HostListener('window:resize')
   onResize() {
-    this.windowWidth = window.innerWidth;
+    if (this.isBrowser) {
+      this.windowWidth = window.innerWidth;
+      this.isSidebarOpen = this.windowWidth >= 640;
+    }
   }
 }
