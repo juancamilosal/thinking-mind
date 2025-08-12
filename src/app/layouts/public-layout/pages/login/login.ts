f8755ecfd1c1935f9f87b38a1db5f29bbb3edd6f
Alejandro Vargas <vargas.yaver@gmail.com>
2025-08-11 20:25:50 -0500

    se crea el formGroup para el login

--- src/app/layouts/public-layout/pages/login/login.ts
+++ src/app/layouts/public-layout/pages/login/login.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
+
 export class Login {
+  private formBuilder = inject(FormBuilder);
+
+  loginForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+
+  onSubmit() {
+      if (this.loginForm.valid) {
+        console.log('Login Form Data:', this.loginForm.value);
+        // login logic
+      } else {
+        console.log('Form is invalid');
+      }
+  }
 
 }
