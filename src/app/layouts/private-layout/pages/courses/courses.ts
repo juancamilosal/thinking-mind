b7c8cd8fb7616878eed871ad60b77cfd9267339a
Alejandro Vargas <vargas.yaver@gmail.com>
2025-08-18 21:34:04 -0500

    Se crea el componente course-info, y se agrega al pg de cursos en front

--- src/app/layouts/private-layout/pages/courses/courses.ts
+++ src/app/layouts/private-layout/pages/courses/courses.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {CourseCardComponent} from '../../../../components/course-card/course-card';
 import {COURSES} from '../../../../core/const/CoursesConst';
+import {CourseInfoComponent} from '../../../../components/course-info/course-info';
 
 @Component({
   selector: 'app-courses',
   imports: [
-    CourseCardComponent
+    CourseCardComponent, CourseInfoComponent
   ],
   templateUrl: './courses.html',
   standalone: true
@@ -13,4 +14,17 @@ import {COURSES} from '../../../../core/const/CoursesConst';
 export class Courses {
 
   protected readonly COURSES = COURSES;
+
+  selectedCourse: any = null;
+  showCourseInfo = false;
+
+  openCourseInfo(course: any) {
+    this.selectedCourse = course;
+    this.showCourseInfo = true;
+  }
+
+  closeCourseInfo() {
+    this.selectedCourse = null;
+    this.showCourseInfo = false;
+  }
 }
