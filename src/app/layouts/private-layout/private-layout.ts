ddcbe6e47a978a169f135fe069c40d0c9159335f
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-08-23 19:04:44 -0500

    Se revierte cambios

--- src/app/layouts/private-layout/private-layout.ts
+++ src/app/layouts/private-layout/private-layout.ts
@@ -1,7 +1,7 @@
 import { Component, HostListener, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
-import { isPlatformBrowser } from "@angular/common";
+import { NgClass, isPlatformBrowser } from "@angular/common";
 import { NotificationModalComponent, NotificationData } from '../../components/notification-modal/notification-modal';
 import { ConfirmationModalComponent, ConfirmationData } from '../../components/confirmation-modal/confirmation-modal';
 import { NotificationService } from '../../core/services/notification.service';
@@ -10,7 +10,7 @@ import { ConfirmationService } from '../../core/services/confirmation.service';
 @Component({
   selector: 'app-private-layout',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, NotificationModalComponent, ConfirmationModalComponent],
+  imports: [RouterOutlet, SidebarComponent, NgClass, NotificationModalComponent, ConfirmationModalComponent],
   templateUrl: './private-layout.html'
 })
 export class PrivateLayout implements OnInit {
