ac0d1411064138d50f4f568d047820a7090f7220
Juan Camilo Salazar Rojas <juancamilosalazarrojas@gmail.com>
2025-08-24 23:21:39 -0500

    Se ajusta app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import {provideRouter, RouteReuseStrategy} from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import {provideHttpClient, withFetch} from '@angular/common/http';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }), // opcional
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withFetch()),
