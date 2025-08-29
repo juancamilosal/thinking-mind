f65925626b30037b0822b491b55876003f6078f4
Alejandro Vargas <vargas.yaver@gmail.com>
2025-08-28 21:02:22 -0500

    se crean tablas de reporte de pagos y estudiantes

--- src/app/layouts/private-layout/pages/reports/reports.ts
+++ src/app/layouts/private-layout/pages/reports/reports.ts
@@ -1,13 +1,56 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, FormsModule } from '@angular/forms';
+import { NotificationService } from '../../../../core/services/notification.service';
+import { ConfirmationService } from '../../../../core/services/confirmation.service';
+import { REPORT_TYPE } from '../../../../core/const/ReportType';
+import { PaymentRecord } from '../../../../core/models/AccountReceivable';
 
 @Component({
   selector: 'app-reports',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './reports.html',
   standalone: true
 })
 
 export class Reports {
+  reportForm!: FormGroup;
+  REPORT_TYPE = REPORT_TYPE;
+  payments: PaymentRecord[] = [];
 
+  constructor(
+    private fb: FormBuilder,
+    private notificationService: NotificationService,
+    private confirmationService: ConfirmationService
+  ) {}
 
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm=(): void => {
+    this.reportForm = this.fb.group({
+      reportType: ['', Validators.required],
+      startDate: [null, Validators.required],
+      endDate: [null, Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.reportForm.valid) {
+      const { reportType, startDate, endDate } = this.reportForm.value;
+
+    }
+  }
+
+  generateReport(): void {
+
+  }
+
+  downloadReport(): void {
+
+  }
+
+  clearForm(): void {
+    this.reportForm.reset();
+  }
 }
