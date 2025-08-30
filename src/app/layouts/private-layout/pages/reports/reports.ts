eaeeb8dba2f1f00d60065dad6a21c6c916de2aab
Alejandro Vargas <vargas.yaver@gmail.com>
2025-08-29 19:20:05 -0500

    se ajustra front, para mostrar reportes con el botón, y estilos de la tabla

--- src/app/layouts/private-layout/pages/reports/reports.ts
+++ src/app/layouts/private-layout/pages/reports/reports.ts
@@ -17,6 +17,7 @@ export class Reports {
   reportForm!: FormGroup;
   REPORT_TYPE = REPORT_TYPE;
   payments: PaymentRecord[] = [];
+  reportGenerated: boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -40,6 +41,7 @@ export class Reports {
   generateReport(): void {
     if (this.reportForm.valid) {
       const { reportType, startDate, endDate } = this.reportForm.value;
+      this.reportGenerated = true;
       this.paymentService.getPayments().subscribe({
       next: (data) => {
         this.payments = data.data;
@@ -59,5 +61,39 @@ export class Reports {
   clearForm(): void {
     this.reportForm.reset();
     this.payments = [];
+    this.reportGenerated = false;
+  }
+
+  formatCurrency(amount: number): string {
+    return new Intl.NumberFormat('es-CO', {
+      style: 'currency',
+      currency: 'COP',
+      minimumFractionDigits: 0
+    }).format(amount);
+  }
+
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('es-CO', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit'
+    }); // Esto devuelve DD/MM/YYYY
+  }
+
+  getStatusColor(estado: string): string {
+    switch (estado) {
+      case 'PAGADO':
+      case 'Completado':
+        return 'bg-green-100 text-green-800';
+      case 'PENDIENTE':
+      case 'Pendiente':
+        return 'bg-orange-100 text-orange-800';
+      case 'RECHAZADO':
+      case 'Cancelado':
+        return 'bg-red-100 text-red-800';
+      default:
+        return 'bg-gray-100 text-gray-800';
+    }
   }
 }
