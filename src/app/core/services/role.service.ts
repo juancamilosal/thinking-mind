863c609eaf3f0430da13c4b5e1e552e459d467f2
Juan Camilo Salazar Rojas <juan.salazar@digitalabus.com>
2025-11-05 21:21:06 -0500

    Se ajusta para crear usuario de finanzas

--- src/app/core/services/role.service.ts
+++ src/app/core/services/role.service.ts
@@ -22,18 +22,19 @@ export class RoleService {
     const allowedRoleIds = [
       'ca89252c-6b5c-4f51-a6e4-34ab4d0e2a02', // Administrador
       'a4ed6390-5421-46d1-b81e-5cad06115abc', // Rector
-      'b40cfe25-bd79-4d62-818b-6cf96674fc12'  // Ventas
+      'b40cfe25-bd79-4d62-818b-6cf96674fc12',  // Ventas
+      'b9cf2164-22ab-4dc8-be5a-8a9420c82f1c' //Finanzas
     ];
-    
+
     const params: any = {
       'fields': 'id,name,description'
     };
-    
+
     // Agregar filtros para cada rol permitido
     allowedRoleIds.forEach((roleId, index) => {
       params[`filter[_or][${index}][id][_eq]`] = roleId;
     });
-    
+
     return this.http.get<ResponseAPI<Role[]>>(this.apiUrl, { params });
   }
 }
