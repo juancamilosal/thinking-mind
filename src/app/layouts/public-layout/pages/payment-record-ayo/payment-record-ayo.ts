3d1d15ca18f3589b9b3ff1e394e6ce2cbf8122e5
Juan Camilo Salazar Rojas <juan.salazar@digitalabus.com>
2025-12-25 23:00:25 -0500

    Se ajusta select de tipo de documento

--- src/app/layouts/public-layout/pages/payment-record-ayo/payment-record-ayo.ts
+++ src/app/layouts/public-layout/pages/payment-record-ayo/payment-record-ayo.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ProgramaAyoService } from '../../../../core/services/programa-ayo.service';
+import { PrecioProgramaAyo } from '../../../../core/models/Course';
+import { DOCUMENT_TYPE } from '../../../../core/const/DocumentTypeConst';
 
 @Component({
     selector: 'app-payment-record-ayo',
@@ -13,10 +16,16 @@ import { RouterModule } from '@angular/router';
 export class PaymentRecordAyoComponent implements OnInit {
     paymentForm!: FormGroup;
     isLoading: boolean = false;
+    precioPrograma: PrecioProgramaAyo | null = null;
+    DOCUMENT_TYPE = DOCUMENT_TYPE;
 
-    constructor(private formBuilder: FormBuilder) { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private programaAyoService: ProgramaAyoService
+    ) { }
 
     ngOnInit(): void {
+        this.loadPrecioPrograma();
         this.paymentForm = this.formBuilder.group({
             tipoDocumento: ['', Validators.required],
             numeroDocumento: ['', Validators.required],
@@ -32,6 +41,19 @@ export class PaymentRecordAyoComponent implements OnInit {
             studentApellido: ['', Validators.required],
             studentEmail: ['', [Validators.required, Validators.email]],
             studentColegio: ['', Validators.required]
+        });
+    }
+
+    loadPrecioPrograma(): void {
+        this.programaAyoService.getPrecioProgramaAyo().subscribe({
+            next: (response) => {
+                if (response.data && response.data.length > 0) {
+                    this.precioPrograma = response.data[0];
+                }
+            },
+            error: (error) => {
+                console.error('Error loading program price:', error);
+            }
         });
     }
 
