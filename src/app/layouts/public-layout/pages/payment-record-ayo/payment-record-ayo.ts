60bf98f1b5fb4e7d17319ae3036c10e1ed0a5ca0
Juan Camilo Salazar Rojas <juan.salazar@digitalabus.com>
2025-12-27 10:17:19 -0500

    Se ajusta la sección de payment-record-ayo

--- src/app/layouts/public-layout/pages/payment-record-ayo/payment-record-ayo.ts
+++ src/app/layouts/public-layout/pages/payment-record-ayo/payment-record-ayo.ts
@@ -84,7 +84,7 @@ export class PaymentRecordAyoComponent implements OnInit {
         this.loadPrecioPrograma();
         this.loadGrupos();
         this.paymentForm = this.formBuilder.group({
-            tipoDocumento: ['', Validators.required],
+            tipoDocumento: ['CC', Validators.required],
             numeroDocumento: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/^[0-9]+$/)]],
             nombre: ['', [Validators.required, Validators.minLength(2)]],
             apellido: ['', [Validators.required, Validators.minLength(2)]],
@@ -92,7 +92,7 @@ export class PaymentRecordAyoComponent implements OnInit {
             email: ['', [Validators.required, Validators.email]],
             direccion: ['', [Validators.required, Validators.minLength(10)]],
             // Student Information
-            studentTipoDocumento: ['', Validators.required],
+            studentTipoDocumento: ['TI', Validators.required],
             studentNumeroDocumento: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/^[0-9]+$/)]],
             studentNombre: ['', [Validators.required, Validators.minLength(2)]],
             studentApellido: ['', [Validators.required, Validators.minLength(2)]],
@@ -504,6 +504,11 @@ export class PaymentRecordAyoComponent implements OnInit {
                 element.scrollIntoView({ behavior: 'smooth', block: 'start' });
             }
         }, 100);
+    }
+
+    cancelAddCourse(): void {
+        this.showAddCourseForm = false;
+        window.scrollTo(0, 0);
     }
 
     private prepareRegisteredCoursesTable(client: any): void {
