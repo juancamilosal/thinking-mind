5dbde57df1db2361e88cca43688db722c6521dfc
Juan Camilo Salazar Rojas <juan.salazar@digitalabus.com>
2026-01-27 21:39:29 -0500

    Se ajusta meet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -31,6 +31,7 @@ import { Advance } from './layouts/private-layout/pages/advance/advance';
 import { Teacher } from './layouts/private-layout/pages/teacher/teacher';
 import { TeacherMeetingsComponent } from './layouts/private-layout/pages/meet/meet-teacher/meet-teacher';
 import {AttendancePageComponent} from './layouts/private-layout/pages/attendance/attendance.component';
+import {MeetComponent} from './layouts/private-layout/pages/meet/meet';
 
 
 export const routes: Routes = [
@@ -173,7 +174,7 @@ export const routes: Routes = [
       {
         path: 'meetings',
         title: 'Thinking Mind | Reuniones Estudiante',
-        component: MeetStudent
+        component: MeetComponent
       },
       {
         path: 'teacher',
