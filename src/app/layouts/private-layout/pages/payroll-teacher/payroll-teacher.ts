c75d18891b0a1716562e3d39d61727725531cd21
Alejandro Vargas <vargas.yaver@gmail.com>
2026-01-31 11:39:48 -0500

    se crea componente nomina profesor

---
+++ src/app/layouts/private-layout/pages/payroll-teacher/payroll-teacher.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payroll-teacher',
+  imports: [],
+  templateUrl: './payroll-teacher.html',
+  styleUrl: './payroll-teacher.css'
+})
+export class PayrollTeacher {
+
+}
