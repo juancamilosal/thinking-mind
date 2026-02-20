2bd1a60465c48d77690cc21c7c519811c07ba918
Juan Camilo Salazar Rojas <juan.salazar@digitalabus.com>
2026-02-20 00:05:21 -0500

    Se mejora la card de usuarios

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
+    path: 'private/accounts-receivable/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
